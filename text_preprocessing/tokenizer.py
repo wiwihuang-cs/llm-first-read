b3ca552b94fa9960a2f665692fdc3e67376323d9
Wiwi Huang <kobeskskk@gmail.com>
2026-01-18 15:30:08 +0800

    Comment the tokenizer.py

--- text_preprocessing/tokenizer.py
+++ text_preprocessing/tokenizer.py
@@ -1,8 +1,8 @@
 '''
 Tokenizer
 
-1. Segmentation
-2. Mapping to token ID
+- Performs token segmentation and maps each token to a uniquo token ID
+- Requires because the embedding layer accepts only integer indices as input
 
 '''
 
@@ -10,15 +10,16 @@ Tokenizer
 import tiktoken  # BPE algorithm by GPT models
 
 
-text = "just for test"
+
 
 # Build a tokenizer
 tokenizer = tiktoken.get_encoding("gpt2")
 
 # Encode the text
+text = "just for test"
 integers = tokenizer.encode(text)
-print(integers)
+# print(integers) # DEBUG: temporary output to verify the tokenizer encoding
 
 # Decode the integers
 strings = tokenizer.decode(integers)
-print(strings)
+# print(strings) # DEBUG: tempoary output to verify the tokenizer decoding
