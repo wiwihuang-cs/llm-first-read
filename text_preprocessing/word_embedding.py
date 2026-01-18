9e8d8bc4cfba4cdfd55d2017933c3a098677ec35
Wiwi Huang <kobeskskk@gmail.com>
2026-01-18 15:47:11 +0800

    Modify word_embedding.py

--- text_preprocessing/word_embedding.py
+++ text_preprocessing/word_embedding.py
@@ -1,6 +1,30 @@
 """
-# === concept ===
-1. embedding : convert data to vector form
-- word2Vec : method of word embedding
-- sentence or paragraph embedding is popular with RAG 
+Embedding layer
+
+- Mapping each token ID to the a uniquo vector 
 """
+from .sliding_window_with_torch import Build_GPTDataLoader
+import torch.nn as nn
+
+txt = "just for test"
+max_length = 4
+dataloader = Build_GPTDataLoader(
+    txt= txt,max_length= max_length, stride= max_length,
+    batch_size= 8, shuffle= False,
+)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+
+# print(inputs)  # temporary output to verify the dataloader
+# print(inputs.shape)
+
+vocab_size = 50257  # GPT-2 vocabulary size
+output_dim = 256
+token_embedding_layer = nn.Embedding(
+    num_embeddings= vocab_size,
+    embedding_dim= output_dim,
+)
+token_embeddings = token_embedding_layer(inputs)
+
+# print(token_embeddings.shape)  # temporary output to verify the embedding layer
