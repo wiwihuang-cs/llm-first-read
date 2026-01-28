9e37745954af3a15bb6281334a0f90c39976cb98
Wiwi Huang <kobeskskk@gmail.com>
2026-01-28 15:24:41 +0800

    refactor: wrapping tokenizer logic into a function

--- text_preprocessing/tokenizer.py
+++ text_preprocessing/tokenizer.py
@@ -1,25 +1,38 @@
-'''
-Tokenizer
+"""
+Tokenizer:
+    Performs token segmentation and maps each token to a unique token ID
+    Requires because the embedding layer accepts only integer indices as input
+"""
 
-- Performs token segmentation and maps each token to a uniquo token ID
-- Requires because the embedding layer accepts only integer indices as input
+# BPE algorithm by GPT models
+import tiktoken  
 
-'''
-
-
-import tiktoken  # BPE algorithm by GPT models
-
-
-
+# Sample input text
+text = "just for test"
 
 # Build a tokenizer
 tokenizer = tiktoken.get_encoding("gpt2")
 
-# Encode the text
-text = "just for test"
-integers = tokenizer.encode(text)
-# print(integers) # DEBUG: temporary output to verify the tokenizer encoding
+def encode_text_to_tokenIDs(texts, tokenizer):
+    """
+    Encode raw texts into tokenID sequences.
 
-# Decode the integers
-strings = tokenizer.decode(integers)
-# print(strings) # DEBUG: tempoary output to verify the tokenizer decoding
+    Args:
+        texts (list of str): raw input texts.
+        tokenizer: tokenizer instance.
+    
+    Returns:
+        list of list of int: token ID sequences.
+    """
+    
+    token_ids = [tokenizer.encode(txt) for txt in texts]
+
+    # DEBUG: temporary output to verify the tokenizer encoding
+    # print(token_ids) 
+
+    raw_texts = [tokenizer.decode(token_id) for token_id in token_ids]
+    
+    # DEBUG: tempoary output to verify the tokenizer decoding
+    # print(raw_texts) 
+
+    return token_ids
