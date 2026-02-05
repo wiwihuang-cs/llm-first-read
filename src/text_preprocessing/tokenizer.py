9c782aef8bfe47325c5aeabecf0bd005f5388766
Wiwi Huang <kobeskskk@gmail.com>
2026-02-05 09:47:59 +0800

    feat: add tokenizer and round-trip tests

--- src/text_preprocessing/tokenizer.py
+++ src/text_preprocessing/tokenizer.py
@@ -1,17 +1,17 @@
 """
 Tokenizer:
-    Performs token segmentation and maps each token to a unique token idx
+    Performs tokens segmentation and maps each token to a unique token id
     Requires because the embedding layer accepts only integer indices as input
 """
 
 def encode_text_to_tokenIDs(texts, tokenizer):
     """
-    Encode raw texts into tokenID sequences.
+    Encode raw texts into tokenIDs.
     
     Returns:
-        list of list of int: token ID sequences.
+        list of list of int
     """
     
-    # The output of tokenizer.encode is a list of token IDs
-    token_idx = [tokenizer.encode(txt) for txt in texts]
-    return token_idx
+    # The output of tokenizer.encode is a list of tokenIDs
+    token_ids = [tokenizer.encode(text) for text in texts]
+    return token_ids
--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -1,16 +1,16 @@
 import tiktoken
 from text_preprocessing.tokenizer import encode_text_to_tokenIDs
 
-# Sample input text
-texts = [
-    "just for test",
-    "another test sentence",
-]
-
-# Build a tokenizer
-tokenizer = tiktoken.get_encoding("gpt2")
-
 def test_tokenizer():
-    token_idx= encode_text_to_tokenIDs(texts= texts, tokenizer= tokenizer)
-    decoded_texts = [tokenizer.decode(token_id) for token_id in token_idx]
+    # Sample input text
+    texts = [
+        "just for test",
+        "another test sentence",
+    ]
+
+    # Build a tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    token_ids= encode_text_to_tokenIDs(texts= texts, tokenizer= tokenizer)
+    decoded_texts = [tokenizer.decode(token_id) for token_id in token_ids]
     assert decoded_texts == texts
